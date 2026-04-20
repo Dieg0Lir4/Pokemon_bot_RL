e42cba8faf0fcaa785790eeb1dba2f02b8ec94ee
Dieg0Lir4 <111295816+Dieg0Lir4@users.noreply.github.com>
2026-04-19 21:16:40 -0600

    feat:guardar progreso

--- src/agents/simple_12f_agent/simple_12f_agent.py
+++ src/agents/simple_12f_agent/simple_12f_agent.py
@@ -9,7 +9,7 @@ class DQNAgent:
         self.gamma = 0.99
         self.epsilon = 1.0
         self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
+        self.epsilon_decay = 0.9975
         self.batch_size = 64
 
         self.memory = deque(maxlen=10000)
--- src/app.py
+++ src/app.py
@@ -1,19 +1,43 @@
 import asyncio
+import csv
 from bots.simple_12f_bot.simple_12f_bot import Simple12fBot
 from enviorment.team.my_team import MY_TEAM
 from poke_env.player import RandomPlayer, SimpleHeuristicsPlayer, MaxBasePowerPlayer
-from poke_env import AccountConfiguration
+
+TOTAL_BATTLES = 100
+VALIDATE_EVERY = 20
+VALIDATION_BATTLES = 50
 
 
 async def main():
     bot1 = Simple12fBot(team=MY_TEAM, battle_format="gen9championsvgc2026regma")
     bot2 = Simple12fBot(team=MY_TEAM, battle_format="gen9championsvgc2026regma")
+    validator = SimpleHeuristicsPlayer(team=MY_TEAM, battle_format="gen9championsvgc2026regma")
 
-    #bot1 = RandomPlayer(team=MY_TEAM, battle_format="gen9championsvgc2026regma")
-    #bot2 = RandomPlayer(team=MY_TEAM, battle_format="gen9championsvgc2026regma")
+    with open("resultados/metricas.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["batalla", "winrate_vs_heuristics", "epsilon"])
+
     print("Iniciando entrenamiento...")
-    await bot1.battle_against(bot2, n_battles=100)
-    print(f"Victorias bot1: {bot1.n_won_battles} de 100")
-    #print(f"Epsilon: {bot1.agent.epsilon:.3f}")
+
+    for i in range(0, TOTAL_BATTLES, VALIDATE_EVERY):
+
+        batalla_actual = i // VALIDATE_EVERY + 1
+
+        await bot1.battle_against(bot2, n_battles=VALIDATE_EVERY)
+
+        bot1.agent.save(f"checkpoints/model_{batalla_actual}.keras")
+
+        wins_before = bot1.n_won_battles
+        await bot1.battle_against(validator, n_battles=VALIDATION_BATTLES)
+        wins_vs_heuristics = bot1.n_won_battles - wins_before
+        winrate = wins_vs_heuristics / VALIDATION_BATTLES * 100
+
+        print(f"Batalla {batalla_actual} | Winrate vs Heuristics: {winrate:.1f}% | Epsilon: {bot1.agent.epsilon:.3f}")
+
+        with open("resultados/metricas.csv", "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([batalla_actual, winrate, bot1.agent.epsilon])
+
 
 asyncio.run(main())
